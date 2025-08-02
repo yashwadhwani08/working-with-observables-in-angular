d4028083d5daad5711d14c142bc9e76143a29672
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-02 14:29:00 +0530

    As evident that what we could achieve with interval() observable, could also be achieve using setInterval and signals and computed (to compensate for pipe-rxjs-operators), but little extra code is required

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -7,14 +15,33 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  interval = signal(0);
+  doubledInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
 
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`);
+    });
+  }
+
   ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(map((val) => val * 2))
-      .subscribe({
-        next: (val) => console.log(val),
-      });
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    const timeInterval = setInterval(() => {
+      console.log(this.doubledInterval());
+      this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1);
+    }, 1000);
+
+    this.destroyRef.onDestroy(() => clearInterval(timeInterval));
+    // const subscription = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
+    // this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
   }
 }
