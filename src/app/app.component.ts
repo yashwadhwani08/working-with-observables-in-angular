ab185943878fee9015e555fca349972d10440922
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-02 16:24:00 +0530

    we could transform a signal into observable using toObservable() from angular/core/rxjs-interop and then we get a Observable on which RxJS operators and subscription could be set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {
   signal,
 } from '@angular/core';
 import { interval, map } from 'rxjs';
-
+import { toObservable } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,29 +16,45 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   clickCount = signal(0);
-  interval = signal(0);
-  doubledInterval = computed(() => this.interval() * 2);
+  clickCount$ = toObservable(this.clickCount);
+  // interval = signal(0);
+  // doubledInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
 
   constructor() {
-    effect(() => {
-      console.log(`Clicked button ${this.clickCount()} times.`);
-    });
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times.`);
+    // });
+    // toObservable(this.clickCount)
   }
 
   ngOnInit(): void {
-    const timeInterval = setInterval(() => {
-      console.log(this.doubledInterval());
-      this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1);
-    }, 1000);
-
-    this.destroyRef.onDestroy(() => clearInterval(timeInterval));
+    // const timeInterval = setInterval(() => {
+    //   console.log(this.doubledInterval());
+    //   this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1);
+    // }, 1000);
+    // this.destroyRef.onDestroy(() => clearInterval(timeInterval));
     // const subscription = interval(1000)
     //   .pipe(map((val) => val * 2))
     //   .subscribe({
     //     next: (val) => console.log(val),
     //   });
     // this.destroyRef.onDestroy(() => subscription.unsubscribe());
+
+    // This approach of passing a function directly to subscribe is a function overloaded version, where we pass the callback function typically passed in next property.
+
+    // this.clickCount$.subscribe((val) => {
+    //   // console.log(`Clicked button ${this.clickCount()} times.`);
+    //   console.log(`Clicked button ${val} times.`);
+    // });
+
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => {
+        console.log(`Clciked button ${val} times.`);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
 
   onClick() {
